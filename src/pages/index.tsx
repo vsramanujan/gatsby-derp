8f1e69a8a895bafebd8c7b8f0af038e64d40a73c
Ramanujan Veeravalli Sevilimedu <rama.psbb@gmail.com>
2022-09-10 17:30:45 +0530

    feat: finish lesson 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import { HeadFC } from "gatsby";
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home">
       <p>Im making this by following the Gatsby tutorial</p>
+      <StaticImage
+        src="../images/icon.png"
+        alt="Derp"
+      />
     </Layout>
   );
 };
